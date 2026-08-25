687f8d5b87636177b60174044ff99e663e2b9ce5
goyalsumit227-sk <goyalsumit227@gmail.com>
2026-08-25 12:50:06 +0530

    new program

--- py_ex-5.py
+++ py_ex-5.py
@@ -1,2 +1,16 @@
 n = int(input("Enter a number: "))
+i=2
 
+if n <= 1:
+    print(n,"is not a prime number")
+        
+while n > 2 and i <n :
+    if n%i == 0:
+
+        print(n,"is not a prime number")
+        i+=1
+      
+      
+
+else:
+    print(n,"is a prime number")
